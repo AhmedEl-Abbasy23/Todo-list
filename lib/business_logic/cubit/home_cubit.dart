72cc71802464c1ac847affee754a9edb8557cfa4
AHMED EL-ABBASY <62981289+AhmedEl-Abbasy23@users.noreply.github.com>
2021-09-11 13:34:32 +0200

    first commit

--- lib/business_logic/cubit/home_cubit.dart
+++ lib/business_logic/cubit/home_cubit.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:todo_list/data/models/task_model.dart';
 
 part 'home_state.dart';
 
